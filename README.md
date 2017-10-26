d962725e5fc9863d04e6d0ea503e9db38a4d98a1
Rishit Rathod <rishitrathod@gmail.com>
2017-10-26 16:36:44 +0530

    Complete

---
+++ README.md
@@ -0,0 +1,1 @@
+This is part of FreeCodeCamp Projects. It returns your IP address, the language and the software you are using.
---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+app.use(device.capture());
+
+app.enable('trust proxy');
+
+app.get('/whoami', function(req, res) {
+    let IP = req.connection.remoteAddress;
+    let ip = req.ip;
+
+    let language = req.headers['accept-language'];
+    language = language.slice(0, language.indexOf(','));
+
+    let user = req.headers['user-agent'];
+    user = user.slice(user.indexOf('(') + 1 , user.indexOf(')'));
+
+    let json = {
+        ipAddress: ip,
+        language: language,
+        software: user
+    }
+    
+    res.json(json);
+})
+
+app.listen(3000, function() {
+    console.log('Server is running 3000');
+})
